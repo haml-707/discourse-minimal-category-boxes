f07450dc6a43baa636035040c81c9078a677babc
haml-707 <845831435@qq.com>
2022-12-01 10:09:36 +0800

    consolog

--- javascripts/discourse/components/custom-category-boxes.js
+++ javascripts/discourse/components/custom-category-boxes.js
@@ -1,15 +1,16 @@
-import Component from "@ember/component";
-import discourseComputed from "discourse-common/utils/decorators";
-import { inject as service } from "@ember/service";
+import Component from '@ember/component';
+import discourseComputed from 'discourse-common/utils/decorators';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  tagName: "div",
+  tagName: 'div',
   router: service(),
-  classNames: ["custom-category-boxes-container"],
-  classNameBindings: ["noneSelected:none-selected"],
+  classNames: ['custom-category-boxes-container'],
+  classNameBindings: ['noneSelected:none-selected'],
   _allowedCategories(selectedCategories) {
+    console.log(service());
     // filters categories to only include selected categories for each section
-    let availableCategories = this.site.categories.filter(category => {
+    let availableCategories = this.site.categories.filter((category) => {
       if (selectedCategories.indexOf(category.id) !== -1) {
         return true;
       } else {
@@ -21,7 +22,7 @@ export default Component.extend({
   },
   @discourseComputed()
   shouldRenderHeadings() {
-    let isCategoryPage = this.router.currentRoute.name.includes("category");
+    let isCategoryPage = this.router.currentRoute.name.includes('category');
     let hasCategoriesSet = false;
     if (
       settings.first_categories ||
@@ -40,36 +41,36 @@ export default Component.extend({
   },
   @discourseComputed()
   noneSelected() {
-    return this.router.currentRoute.name.includes("None");
+    return this.router.currentRoute.name.includes('None');
   },
   @discourseComputed()
   firstCategories() {
     return this._allowedCategories(
-      settings.first_categories.split("|").map(id => Number(id))
+      settings.first_categories.split('|').map((id) => Number(id))
     );
   },
   @discourseComputed()
   secondCategories() {
     return this._allowedCategories(
-      settings.second_categories.split("|").map(id => Number(id))
+      settings.second_categories.split('|').map((id) => Number(id))
     );
   },
   @discourseComputed()
   thirdCategories() {
     return this._allowedCategories(
-      settings.third_categories.split("|").map(id => Number(id))
+      settings.third_categories.split('|').map((id) => Number(id))
     );
   },
   @discourseComputed()
   fourthCategories() {
     return this._allowedCategories(
-      settings.fourth_categories.split("|").map(id => Number(id))
+      settings.fourth_categories.split('|').map((id) => Number(id))
     );
   },
   @discourseComputed()
   fifthCategories() {
     return this._allowedCategories(
-      settings.fifth_categories.split("|").map(id => Number(id))
+      settings.fifth_categories.split('|').map((id) => Number(id))
     );
-  }
+  },
 });
--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -1,9 +1,9 @@
-import { registerUnbound } from "discourse-common/lib/helpers";
-import { withPluginApi } from "discourse/lib/plugin-api";
-import Category from "discourse/models/category";
+import { registerUnbound } from 'discourse-common/lib/helpers';
+import { withPluginApi } from 'discourse/lib/plugin-api';
+import Category from 'discourse/models/category';
 
-registerUnbound("getAbbreviation", categoryName => {
-  let abbr = categoryName.replace(" and", "").split(" ");
+registerUnbound('getAbbreviation', (categoryName) => {
+  let abbr = categoryName.replace(' and', '').split(' ');
 
   if (abbr.length > 1) {
     abbr = abbr[0].charAt(0).toUpperCase() + abbr[1].charAt(0).toLowerCase();
